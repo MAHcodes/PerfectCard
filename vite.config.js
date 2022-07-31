527e1832baaa89649ce854953c58fcb59b0d62e9
mhmdali102 <mhmdali102@bk.ru>
2022-08-01 02:57:16 +0300

    fix vite build base path issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-// import reactRefresh from "@vitejs/plugin-react-refresh";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: '',
 });
